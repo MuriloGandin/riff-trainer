30af2bb984641472c4196bdd45afec9da3e5eda6
murilo <muriloandradegandin@gmail.com>
2026-03-12 20:37:41 -0300

    added midi API

---
+++ static/js/audio.js
@@ -0,0 +1,8 @@
+document.querySelector("#play").addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+
+    Tone.Transport.bpm.value = 120;
+    synth.triggerAttackRelease("C4", "8n", now);
+});

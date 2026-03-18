b62676cb3ec76dd0d638b64dc87047ee35aa1b37
murilo <muriloandradegandin@gmail.com>
2026-03-18 20:00:24 -0300

    minor cursor update

--- static/js/cursorSync.js
+++ static/js/cursorSync.js
@@ -1,6 +1,17 @@
-let cursorPB = document.querySelector(".playback-cursor");
-const container = document.querySelector(".tab-container");
+window.addEventListener("load", () => {
+    setTimeout(() => {
 
-const width = container.clientWidth;
+        let cursorPB = document.querySelector(".playback-cursor");
 
-console.log(window.data)
+        const container = document.querySelector(".tab-container");
+        const containerRect = container.getBoundingClientRect();
+
+        const tabElement = document.querySelector("#notation canvas, #notation svg");
+        const rect = tabElement.getBoundingClientRect();
+
+        const startX = rect.left - containerRect.left;
+        console.log("startX:", startX);
+        cursorPB.style.left = `${startX}px`;
+
+    }, 100);
+});

899e9ef6c85b01756b0c0b1766fb9438f6b268cf
Murilo <muriloandradegandin@gmail.com>
2026-03-19 11:53:48 -0300

    initial cursor movement

--- static/js/cursorSync.js
+++ static/js/cursorSync.js
@@ -1,3 +1,7 @@
+// This file makes the tablature cursor work in sync with the audio
+
+// Initial cursor position
+
 window.addEventListener("load", () => {
     setTimeout(() => {
 
@@ -6,12 +10,39 @@ window.addEventListener("load", () => {
         const container = document.querySelector(".tab-container");
         const containerRect = container.getBoundingClientRect();
 
-        const tabElement = document.querySelector("#notation canvas, #notation svg");
+        const tabElement = document.querySelector("#notation");
         const rect = tabElement.getBoundingClientRect();
 
-        const startX = rect.left - containerRect.left;
+        const startX = rect.left - containerRect.left + 50;
         console.log("startX:", startX);
         cursorPB.style.left = `${startX}px`;
+
+        // Defines the allowed position for cursor
+        const tabWidth = tabElement.clientWidth;
+        const endX = startX + tabWidth;
+
+        // PLACEHOLDER
+        const totalDuration = 20;
+
+        function timeToX(currentTime) {
+            const progress = currentTime / totalDuration;
+            return startX + progress * tabWidth;
+        }
+
+        function updateCursor() {
+            const currentTime = Tone.Transport.seconds;
+
+            const x = timeToX(currentTime)
+            cursorPB.style.left = `${x}px`;
+
+            requestAnimationFrame(updateCursor)
+        }
+
+        document.querySelector("#preview").addEventListener("click", () => {
+            console.log("Cursor active")
+            updateCursor();
+        })
+
 
     }, 100);
 });

665602a839055f17885ede620d07e70d2a6bc746
Murilo <muriloandradegandin@gmail.com>
2026-04-01 11:58:26 -0300

    added riff name formating for homepage

--- static/js/visuals.js
+++ static/js/visuals.js
@@ -14,3 +14,25 @@ search.addEventListener("input", function() {
         }
     });
 });
+
+// Format riff names for display
+function formatRiffName(name) {
+    return name
+    // CamelCase to normal
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    // Capitalize first letter of each word
+    .replace(/\b\w/g, char => char.toUpperCase())
+    // Separate words followed by numbers
+    .replace(/([a-zA-Z])(\d)/g, '$1 $2')
+    // Replace underscores with spaces
+    .replace(/_/g, ' ');
+}
+
+const riffButtons = document.querySelectorAll(".riff-item button");
+
+riffButtons.forEach(button => {
+    const originalText = button.innerText;
+    const formattedText = formatRiffName(originalText);
+
+    button.innerText = formattedText;
+});
